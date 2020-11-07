8935e53999a3ee1d95bf40d2e8577b5d656779df
EslamShaban <eshaban242@gmail.com>
2020-11-07 22:12:31 +0200

    install front theme

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,8 @@
+@include('front.layout.header');
+@include('front.layout.navbar');
+@include('front.layout.menu');
+@include('front.layout.message');
+
+@yield('content');
+
+@include('front.layout.footer');
